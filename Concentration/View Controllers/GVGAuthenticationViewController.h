122d5002edc8c44a1139929638fe183a19f3ff3b
Giles Van Gruisen <gilesvangruisen@me.com>
2014-06-27 14:08:42 -0400

    empty auth view controller

---
+++ Concentration/View Controllers/GVGAuthenticationViewController.h
@@ -0,0 +1,13 @@
+//
+//  GVGAuthenticationViewController.h
+//  Concentration
+//
+//  Created by Giles Van Gruisen on 6/27/14.
+//  Copyright (c) 2014 Fetchnotes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GVGAuthenticationViewController : UIViewController
+
+@end
