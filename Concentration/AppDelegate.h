31c2abf8ed0ed1a73a475edca25a1ada18c5c420
Giles Van Gruisen <gilesvangruisen@me.com>
2014-06-27 12:51:09 -0400

    init empty xcode project

---
+++ Concentration/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Concentration
+//
+//  Created by Giles Van Gruisen on 6/27/14.
+//  Copyright (c) 2014 Fetchnotes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Concentration/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Concentration
+//
+//  Created by Giles Van Gruisen on 6/27/14.
+//  Copyright (c) 2014 Fetchnotes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
