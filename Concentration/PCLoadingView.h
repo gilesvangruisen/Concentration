3f69824673fbb7c7b79be9eeae9fbf51a65a1c4d
Giles Van Gruisen <gilesvangruisen@me.com>
2014-06-28 16:46:59 -0400

    loading spinner steps

---
+++ Concentration/PCLoadingView.h
@@ -0,0 +1,16 @@
+//
+//  PCLoadingView.h
+//  Remarkable
+//
+//  Created by Giles Van Gruisen on 4/28/14.
+//  Copyright (c) 2014 Giles Van Gruisen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PCLoadingView : UIView
+
+- (void)fadeIn;
+- (void)fadeOut;
+
+@end
